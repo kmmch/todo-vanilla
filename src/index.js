09cc1d483e92fceca6606935624071ef2eb5ec5b
kmmch <kmmchmsy090211@gmail.com>
2023-04-18 20:31:12 +0900

    追加ボタン作成

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
@@ -8,7 +6,32 @@ const onClickAdd = () => {
   // li生成
   const li = document.createElement("li");
   li.className = "row";
-  alert(li);
+  
+  // p生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // 完了ボタン生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // 削除ボタン生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // liの子要素にp, 完了ボタン, 削除ボタンを追加
+  li.appendChild(p);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  // 未完了のTODOに追加
+  document.getElementById("imcomplete-list").appendChild(li);
 };
 
 document
